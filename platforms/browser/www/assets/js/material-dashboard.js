5ececc875cb3ee1972c012278081e42b9c3cf091
ThangPC <thangvnnc@gmail.com>
2019-07-11 23:58:27 +0700

    commit

--- platforms/browser/www/assets/js/material-dashboard.js
+++ platforms/browser/www/assets/js/material-dashboard.js
@@ -164,25 +164,6 @@ md = {
     }
   },
 
-  showNotification: function(from, align) {
-    type = ['', 'info', 'danger', 'success', 'warning', 'rose', 'primary'];
-
-    color = Math.floor((Math.random() * 6) + 1);
-
-    $.notify({
-      icon: "add_alert",
-      message: "Welcome to <b>Material Dashboard Pro</b> - a beautiful admin panel for every web developer."
-
-    }, {
-      type: type[color],
-      timer: 3000,
-      placement: {
-        from: from,
-        align: align
-      }
-    });
-  },
-
   initDocumentationCharts: function() {
     if ($('#dailySalesChart').length != 0 && $('#websiteViewsChart').length != 0) {
       /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
--- www/assets/js/plugins/common.js
+++ www/assets/js/plugins/common.js
@@ -1,1 +1,16 @@
 let URLBase = "http://hongphat.tinhlaisuat.info";
+// let URLBase = "http://local.com";
+
+let DBcustomers = localStorage.getItem('db_customers');
+let DBusers = localStorage.getItem('db_users');
+
+if(DBcustomers === null && DBusers === null) {
+    updateDatabase();
+}
+
+function updateDatabase() {
+
+}
+
+
+
